672d07761e3a428f7a2e977e54e7e06014a9d7ac
AngrySushiroll8 <MTMassey@student.fullsail.edu>
2025-08-10 11:55:40 -0400

    Simple moving platform and parent triggers. Took a little longer to actually nail the positioning of the hitbox for the tests.

---
+++ Team Hemotopia/Assets/Scripts/CharacterTriggerEvents.cs
@@ -0,0 +1,13 @@
+using UnityEngine;
+
+public class CharacterTriggerEvents : MonoBehaviour
+{
+    private void OnTriggerEnter(Collider other)
+    {
+        other.transform.parent = gameObject.transform;
+    }
+    private void OnTriggerExit(Collider other)
+    {
+        other.transform.parent = null;
+    }
+}
