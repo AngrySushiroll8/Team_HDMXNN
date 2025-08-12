a23113536c14b32749c9c683a403f34ee3098759
Nicolas Anestor <nmanestor@student.fullsail.edu>
2025-08-12 15:26:13 -0400

    weapon full implementation

--- Team Hemotopia/Assets/Scripts/PlayerController.cs
+++ Team Hemotopia/Assets/Scripts/PlayerController.cs
@@ -409,25 +409,31 @@ public class PlayerController : MonoBehaviour, IDamage
             case 1: // pistol
                 {
                     weapon = Weapon.Pistol;
-
+                    HideAllWeapons();
+                    pistolModel.gameObject.SetActive(true);
                     break;
                 }
 
             case 2: // Assault Rifle
                 {
                     weapon = Weapon.AssaultRifle;
+                    HideAllWeapons();
+                    assaultRifleModel.gameObject.SetActive(true);
                     break;
                 }
 
             case 3: // Shotgun
                 {
                     weapon = Weapon.Shotgun;
+                    HideAllWeapons();
+                    shotgunModel.gameObject.SetActive(true);
                     break;
                 }
 
             case 4: //Axe
                 {
                     weapon = Weapon.Axe;
+                    HideAllWeapons();
                     axeModel.gameObject.SetActive(true);
                     break;
                 }
@@ -515,6 +521,53 @@ public class PlayerController : MonoBehaviour, IDamage
         }
     }
 
+    void HideAllWeapons()
+    {
+        pistolModel.gameObject.SetActive(false);
+        assaultRifleModel.gameObject.SetActive(false);
+        shotgunModel.gameObject.SetActive(false);
+        axeModel.gameObject.SetActive(false);
+    }
+
+    //void ShowPistol(bool show)
+    //{
+    //    if(show == true)
+    //    {
+    //        pistolModel.gameObject.SetActive(true);
+            
+    //    }
+    //    else
+    //    {
+    //        pistolModel.gameObject.SetActive(false);
+    //    }
+    //}
+
+    //void ShowRifle(bool show)
+    //{
+    //    if (show == true)
+    //    {
+    //        assaultRifleModel.gameObject.SetActive(true);
+
+    //    }
+    //    else
+    //    {
+    //        assaultRifleModel.gameObject.SetActive(false);
+    //    }
+    //}
+
+    //void ShowShotgun(bool show)
+    //{
+    //    if (show == true)
+    //    {
+    //        shotgunModel.gameObject.SetActive(true);
+
+    //    }
+    //    else
+    //    {
+    //        //shotgunModel.
+    //       // shotgunModel.gameObject.SetActive(false);
+    //    }
+    //}
 
 }
 
