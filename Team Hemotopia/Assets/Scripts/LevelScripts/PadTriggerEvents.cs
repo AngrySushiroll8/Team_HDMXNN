9b0f9e646a3fd3cad1b11371c11e34ec79cef087
Xavier Meneses <xavier@ballards.org>
2025-08-20 17:51:38 -0400

    Jump Pad 70/30
    Working 70% of the time

--- Team Hemotopia/Assets/Scripts/LevelScripts/PadTriggerEvents.cs
+++ Team Hemotopia/Assets/Scripts/LevelScripts/PadTriggerEvents.cs
@@ -1,28 +1,38 @@
-using System;
-using Unity.VisualScripting;
 using UnityEngine;
 
 public class PadTriggerEvents : MonoBehaviour
 {
     [SerializeField] int jumpHeight;
+    [SerializeField] int slowSpeed;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
     }
     private void OnTriggerEnter(Collider collider)
     {
         //Extra teleport line for later
         //GameManager.instance.player.transform.position = new Vector3(currentPos.x, currentPos.y + jumpHeight, currentPos.z);
-        
+
         // 50/50 Jump Pad
-        GameManager.instance.player.GetComponent<PlayerController>().jumpVec.y = jumpHeight;
+        if (this.CompareTag("jumpPad"))
+        {
+            while (true)
+            {
+                GameManager.instance.player.GetComponent<PlayerController>().jumpVec.y = jumpHeight;
+                if (GameManager.instance.player.GetComponent<PlayerController>().jumpVec.y != 0) break;
+            }
+        }
+        else if (this.CompareTag("slowField"))
+        {
+            
+        }
     }
 }
