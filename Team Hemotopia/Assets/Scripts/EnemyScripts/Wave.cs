e24b446fc372d4b725bf610ca90a366932e3ea09
HunterHuelsmann <hunter.huelsmann07@gmail.com>
2025-08-26 14:41:26 -0400

    Waves Fixed With Flying Enemy Spawns

--- Team Hemotopia/Assets/Scripts/EnemyScripts/Wave.cs
+++ Team Hemotopia/Assets/Scripts/EnemyScripts/Wave.cs
@@ -1,3 +1,4 @@
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
@@ -7,11 +8,14 @@ public class Wave : MonoBehaviour
     [SerializeField] GameObject[] enemyPrefabs;
     [Tooltip("Must be the same length as the prefabs.")]
     [SerializeField] int[] enemyAmounts;
+    [SerializeField] int flyingEnemyHeight;
 
     Transform[] spawnPositions;
     Dictionary<GameObject, int> waveEnemies = new Dictionary<GameObject, int>();
-    
+
     public GameObject positionContainer;
+    List<GameObject> enemiesToSpawn = new List<GameObject>();
+    float enemyTimer = 0;
 
     void Start()
     {
@@ -27,13 +31,29 @@ public class Wave : MonoBehaviour
         }
     }
 
+    void Update()
+    {
+        if (enemiesToSpawn.Count > 0)
+        {
+            enemyTimer += Time.deltaTime;
+            if (enemyTimer > 0.15f)
+            {
+                int spawnIndex = Random.Range(0, spawnPositions.Length - 1);
+                Instantiate(enemiesToSpawn[0], spawnPositions.Length > 0 ? (enemiesToSpawn[0].CompareTag("FlyingEnemy") ? spawnPositions[spawnIndex].position + (spawnPositions[spawnIndex].up * flyingEnemyHeight) : spawnPositions[spawnIndex].position) : transform.position, Quaternion.identity, null);
+                enemiesToSpawn.RemoveAt(0);
+                enemyTimer = 0;
+                Debug.Log("Spawn");
+            }
+        }
+    }
+
     public void StartWave()
     {
         foreach (KeyValuePair<GameObject, int> pair in waveEnemies)
         {
             for (int enemyIndex = 0; enemyIndex < pair.Value; enemyIndex++)
             {
-                Instantiate(pair.Key, spawnPositions.Length > 0 ? spawnPositions[Random.Range(0, spawnPositions.Length - 1)].position : transform.position, Quaternion.identity, null);
+                enemiesToSpawn.Add(pair.Key);
             }
         }
     }
