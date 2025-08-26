16f6bff86ab5526f821bf5aea106bdb96af9a329
HunterHuelsmann <hunter.huelsmann07@gmail.com>
2025-08-26 13:33:56 -0400

    Fixed Rage Damage

--- Team Hemotopia/Assets/Scripts/PlayerScripts/PlayerController.cs
+++ Team Hemotopia/Assets/Scripts/PlayerScripts/PlayerController.cs
@@ -144,9 +144,6 @@ public class PlayerController : MonoBehaviour, IDamage, IPickup
 
         updatePlayerUI();
 
-
-       
-
         damageOriginal = damage;
         rageSpeed = speed * 1.5f;
         rageDamage = (int)(damage * 1.5f);
@@ -759,6 +756,8 @@ public class PlayerController : MonoBehaviour, IDamage, IPickup
         bloomMod = gunList[gunListPos].bloomMod;
         rageMeterIncrement = gunList[gunListPos].rageMeterIncrement;
         damage = gunList[gunListPos].damage;
+        damageOriginal = damage;
+        rageDamage = gunList[gunListPos].rageDamage;
 
         gunModel.GetComponent<MeshFilter>().sharedMesh = gunList[gunListPos].model.GetComponent<MeshFilter>().sharedMesh;
         gunModel.GetComponent<MeshRenderer>().sharedMaterial = gunList[gunListPos].model.GetComponent<MeshRenderer>().sharedMaterial;
